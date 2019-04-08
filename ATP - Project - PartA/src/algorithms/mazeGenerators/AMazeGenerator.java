70d55a9091c82fc95383f671e0ad05bd7c51e8bd
shamaid <49365828+shamaid@users.noreply.github.com>
2019-04-08 18:32:45 +0300

    Update AMazeGenerator.java

--- ATP - Project - PartA/src/algorithms/mazeGenerators/AMazeGenerator.java
+++ ATP - Project - PartA/src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -1,11 +1,31 @@
 package algorithms.mazeGenerators;
-
-///test-test
+/**
+ *AMazeGenerator class is an abstract class.
+ * This class creates the template to generate a maze.
+ *It has an abstract method that generate a maze (The classes that inherit this class implement the function).
+ * This class implements the Interface IMazeGenerator.
+ */
 
 public abstract class AMazeGenerator implements IMazeGenerator {
 
+    /**
+     * This is an abstract method that generate a maze.
+     * (The classes that inherit AMazeGenerator must execute this method to create an instance)
+     * @param row this is the number of rows in the maze.
+     * @param column this is the number of columns in the maze.
+     * @return Maze. This returns the maze that was generated in the method.
+     */
     public abstract Maze generate (int row, int column);
 
+
+
+    /**
+     * This method measures the system clock before generate the maze and after generate the maze.
+     * And calculates how long it took to produce the maze.
+     * @param row this is the number of rows in the maze.
+     * @param column this is the number of columns in the maze.
+     * @return long. This returns the time that took to generate the maze.
+     */
     public long measureAlgorithmTimeMillis (int row, int column){
         long startTime = System.currentTimeMillis();
         generate(row,column);
