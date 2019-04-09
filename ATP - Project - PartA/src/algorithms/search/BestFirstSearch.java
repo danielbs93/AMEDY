7ca5c08dd151a9c00f1ca5228070b7b40daab28c
michaltalmor <49365593+michaltalmor@users.noreply.github.com>
2019-04-09 18:00:51 +0300

    Update BestFirstSearch.java
    uploading javaDoc

--- ATP - Project - PartA/src/algorithms/search/BestFirstSearch.java
+++ ATP - Project - PartA/src/algorithms/search/BestFirstSearch.java
@@ -4,15 +4,28 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
+/**
+ * This class is for best First Search algorithm
+ * Extends from BreadthFirstSearch class
+ */
 public class BestFirstSearch extends BreadthFirstSearch {
 
 
+    /**
+     * Default constructor
+     *
+     */
     public BestFirstSearch() {
         visitedPosition = new HashSet<>();
         statesQueue = new PriorityQueue<>(AState::compareTo);
     }
 
-    @Override
+    /**
+     * Getter for algorithm name
+     * @return String - "Best First Search"
+     *
+     */
+
     public String getName() {
         return "Best First Search";
     }
